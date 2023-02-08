8703c828d569cf34184eec2087c67bcb6447fc61
Pranav Vashi <neobuddy89@gmail.com>
2023-02-09 01:44:31 +0530

    Launcher3: Add customization to set hotseat background opacity
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -173,6 +173,7 @@ public final class Utilities {
     public static final String KEY_RECENTS_OPACITY = "pref_recents_opacity";
     public static final String KEY_APP_DRAWER_OPACITY = "pref_app_drawer_opacity";
     public static final String KEY_DRAWER_SEARCH = "pref_drawer_search";
+    public static final String KEY_HOTSEAT_OPACITY = "pref_hotseat_opacity";
 
     /**
      * Returns true if theme is dark.
@@ -910,5 +911,10 @@ public final class Utilities {
     public static boolean showSearch(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DRAWER_SEARCH, true);
+    }
+
+    public static int getHotseatOpacity(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getInt(KEY_HOTSEAT_OPACITY, 40);
     }
 }
