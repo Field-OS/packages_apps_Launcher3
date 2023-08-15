968c5f88c8a42c84a230fd749e791a1431bc95d1
minaripenguin <minaripenguin@users.noreply.github.com>
2023-08-15 14:12:10 +0800

    Launcher3: Disable live tile feature
    * conflicts with overview scrolling and somehow is connected with stucked recents view when using gestures
    * add workarounds for gestures cancel scenario
    
    Change-Id: I9a56ec78f74a88b99d8bba5d17c5db321debac9c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
+++ quickstep/src/com/android/launcher3/uioverrides/QuickstepLauncher.java
@@ -755,10 +755,6 @@ public class QuickstepLauncher extends Launcher {
     @Override
     protected void onScreenOnChanged(boolean isOn) {
         super.onScreenOnChanged(isOn);
-        if (!isOn) {
-            RecentsView recentsView = getOverviewPanel();
-            recentsView.finishRecentsAnimation(true /* toRecents */, null);
-        }
     }
 
     @Override
